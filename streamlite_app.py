c3d26f80119a147294d2ac9f59d68f186e2f7c9e
Celeste Sandoval <cel5209@gmail.com>
2023-01-18 22:35:23 -0600

    Update streamlite_app.py
    New titles and emojis

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 st.title('Healthy food')
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+ 
